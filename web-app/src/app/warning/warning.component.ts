f02937842c75cd3c1c4aeb8b0ffc9df95fa6580f
Dulanjalie Adhikari <dula.adhi@gmail.com>
2018-10-22 18:03:32 +1000

    Added more compoenets

---
+++ web-app/src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning',
+    templateUrl: './warning.component.html',
+    styles: [`
+        p{
+            color: yellow
+        }
+    `]
+})
+export class WarningComponent{}
